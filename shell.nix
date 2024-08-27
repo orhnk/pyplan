8a6dc631ae4179f70ec0f5f8b01596ebfa18f71b
orhnk <utfeightt@gmail.com>
2024-08-27 04:59:19 +0300

    build: add shell.nix

---
+++ shell.nix
@@ -0,0 +1,16 @@
+let
+  # We pin to a specific nixpkgs commit for reproducibility.
+  # Last updated: 2024-04-29. Check for new commits at https://status.nixos.org.
+  pkgs = import (fetchTarball "https://github.com/NixOS/nixpkgs/archive/8a4ba48accedd56f7585ccd452a47be6f80da60e.tar.gz") {};
+in
+  pkgs.mkShell {
+    packages = [
+      (pkgs.python3.withPackages (pypkgs:
+        with pypkgs; [
+          # select Python packages here
+          google-api-python-client
+          google-auth-httplib2
+          google-auth-oauthlib
+        ]))
+    ];
+  }
