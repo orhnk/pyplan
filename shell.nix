d7449a79adf4748378f5e550b50718c2339d75cc
orhnk <utfeightt@gmail.com>
2024-09-03 05:22:09 +0300

    build: update package inputs

--- shell.nix
+++ shell.nix
@@ -4,13 +4,17 @@ let
   pkgs = import (fetchTarball "https://github.com/NixOS/nixpkgs/archive/8a4ba48accedd56f7585ccd452a47be6f80da60e.tar.gz") {};
 in
   pkgs.mkShell {
-    packages = [
-      (pkgs.python3.withPackages (pypkgs:
+    packages = with pkgs; [
+      pyright
+      ruff
+
+      (python3.withPackages (pypkgs:
         with pypkgs; [
           # select Python packages here
           google-api-python-client
           google-auth-httplib2
           google-auth-oauthlib
+          python-dateutil
         ]))
     ];
   }
